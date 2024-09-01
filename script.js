5a001fae07827f52b6d2ef68e0d11b0690c10452
kaustubhlearner <kaustubhpandey81788@gmail.com>
2024-09-01 12:06:34 +0530

    learning

---
+++ script.js
@@ -0,0 +1,132 @@
+
+// let a = document.querySelector('#input')
+
+
+// a.addEventListener('click',() =>{
+//     console.log("clicked");
+
+// })
+
+
+
+// a.addEventListener('dblclick',() =>{
+//     console.log("clicked");
+
+// })
+
+
+// let a = document.getElementById("name")
+// console.log(a);
+
+// let a = document.querySelector("div");
+// // a.innerHTML+= "<input type='text'> hello world</input>"
+// console.log(a.outerHTML);
+
+
+
+// let b = document.querySelector("p");
+// b.innerHTML = "<input type='text'></input>"
+// console.log(b.innerHTML);
+
+
+// let a =document.createElement("h1")
+// a.setAttribute("class", "app")
+// a.textContent = "misra apna beta hai"
+// console.log(a);
+
+// document.body.prepend(a)
+
+
+// let green= document.querySelector(".green")
+// let pink= document.querySelector(".pink")
+// let blue= document.querySelector(".blue")
+
+// green.addEventListener('click', (e)=>{
+//     e.stopPropagation()
+//     console.log("green clicked");
+
+// })
+
+// pink.addEventListener('click', (e)=>{
+//     e.stopPropagation()
+//     console.log("pink clicked");
+
+// })
+
+// blue.addEventListener('click', (e)=>{
+//     e.stopPropagation()
+//     console.log("blue clicked");
+
+// })
+
+// document.addEventListener('click', (e)=>{
+//     console.log("document clicked");
+
+// })
+
+// window.addEventListener('click', (e)=>{
+//     console.log("window clicked");
+
+// })
+
+
+
+// let ul = document.createElement("ul");
+// while(true){
+// let input = prompt("enter the input");
+// if(input==="" || input===null)
+//     break;
+// let li = document.createElement("li");
+// ul.append(li)
+// li.textContent = input;
+// }
+// document.body.append(ul)
+
+
+
+// let input = prompt("enter the number");
+// let ul = document.createElement("ul")
+// let li = document.createElement("li")
+// li.textContent= input;
+// ul.append(li)
+// document.body.append(ul)
+
+// let ul = document.createElement("ul")
+// while (true){
+// let input = prompt("Enter the input")
+// if (input == "" || input === null) 
+//     break;
+// let li = document.createElement("li");
+// ul.append(li)
+// li.textContent = input
+// }
+// document.body.append(ul)
+
+// let p = document.querySelector("p");
+// for (const element of p.classList) {
+//     console.log(element);
+
+// }
+
+// let button = document.querySelector("#button");
+
+// function runcode(){
+//     console.log("hello!");
+
+// }
+
+// button.onclick = runcode
+
+// button.addEventListener("click", function (){
+//     console.log("namste");
+
+// })
+
+// button.addEventListener("click",runcode)
+// function runcode (){
+    //     console.log("namste");
+    
+    // }
+    
+    
+    // c
